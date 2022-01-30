1e04b9f0062d9288ee4ea1f491015c5ef608285a
chasebarn35 <chasebarn35@gmail.com>
2022-01-29 23:47:06 -0600

    I braindead

--- scanTweets.py
+++ scanTweets.py
@@ -1,4 +1,5 @@
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from instagramy import InstagramUser,InstagramPost
 import twint
 import csv
 
@@ -6,26 +7,13 @@ def sentiment_scores(sentence): #fullscale copied from geek4geek omegalul
  
     # Create a SentimentIntensityAnalyzer object.
     sid_obj = SentimentIntensityAnalyzer()
- 
-    # polarity_scores method of SentimentIntensityAnalyzer
-    # object gives a sentiment dictionary.
-    # which contains pos, neg, neu, and compound scores.
+
     sentiment_dict = sid_obj.polarity_scores(sentence)
-     
-    # print("Overall sentiment dictionary is : ", sentiment_dict)
-    # print("sentence was rated as ", sentiment_dict['neg']*100, "% Negative")
-    # print("sentence was rated as ", sentiment_dict['neu']*100, "% Neutral")
-    # print("sentence was rated as ", sentiment_dict['pos']*100, "% Positive")
- 
-    # print("Sentence Overall Rated As", end = " ")
- 
-    # decide sentiment as positive, negative and neutral
+
     if sentiment_dict['compound'] >= 0.05 :
         return "Positive"
- 
     elif sentiment_dict['compound'] <= - 0.05 :
         return "Negative"
- 
     else :
         return "Neutral"
 
@@ -42,28 +30,37 @@ def TweetGrab(TweetCount, username):
     twint.run.Search(c)
 #-----------------------------------------
 
-#-----------------------------------------TODO Facebook Data Collection
-
+#-----------------------------------------TODO Instagram Data Collection
+def InstaGrab(username):
+    user = InstagramUser(username, from_cache = True) ##TODO MAKE SURE THE CACHE IS WORKING
+    for x in user.posts:
+        with open(username+"Instagram.csv",'w', encoding ='UTF8') as csvfile:
+            writer = csv.writer(csvfile)
+            print(InstagramPost(x.shortcode,from_cache = True).text)
+            writer.writerows(InstagramPost(x.shortcode,from_cache = True).text)
 
 
 #-----------------------------------------
 
-#--------------------------------------- TODO Parse through each tweet and determine if it's "good" or bad
-User = ""
-TweetGrab(100,User)
-sumPos = 0
-sumNeg = 0
-sumNeu = 0
-with open(User+"Twitter.csv", newline='') as csvfile:
-    reader = csv.reader(csvfile, delimiter='\n', quotechar='|')
-    for row in reader:
-        tempans = sentiment_scores(row)
-        if(tempans == "Positive"):
-            sumPos+=1
-        if(tempans == "Negative"):
-            sumNeg+=1
-        if(tempans == "Neutral"):
-            sumNeu+=1
-print(str(sumPos) + " positive Tweets")
-print(str(sumNeg) + " negative Tweets")
-print(str(sumNeu) + " neutral Tweetss")
+#--------------------------------------- Parse through each tweet and determine if it's "good" or bad. this is not super great
+def TweetRate(User):
+    TweetGrab(100,User)
+    sumPos = 0
+    sumNeg = 0
+    sumNeu = 0
+    with open(User+"Twitter.csv", newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter='\n', quotechar='|')
+        for row in reader:
+            tempans = sentiment_scores(row)
+            if(tempans == "Positive"):
+                sumPos+=1
+            if(tempans == "Negative"):
+                sumNeg+=1
+            if(tempans == "Neutral"):
+                sumNeu+=1
+    print(str(sumPos) + " positive Tweets")
+    print(str(sumNeg) + " negative Tweets")
+    print(str(sumNeu) + " neutral Tweetss")
+#------------------------------------------
+
+InstaGrab('taylorswift')
